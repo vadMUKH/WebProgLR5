6a10290eed8c639eb293f448b9be235bccf758c3
Anton <toxa1015@gmail.com>
2020-12-04 19:03:43 +0500

    Index
    index-page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>МухамедьяровВА_ИП_ЛР</title>
+</head>
+<body>
+    <h1> Мухамедьяров Вадим А.</h1>
+    
+    <h2> Лабораторная работа №1</h2>
+    
+  <ul>
+    <li><a href="Mukhamedyarov/WWW/lab1-5.php" >Задание №1</a>
+    <li><a href="Mukhamedyarov/WWW/lab-2-10.php" >Задание №2</a>
+    <li><a href="Mukhamedyarov/WWW/lab-2-11.php" >Задание №3</a>
+  </ul>
+  
+    <h2>Лабораторная работа №2</h2>
+    
+  <ul>
+    <li><a href="Mukhamedyarov/WWW/lab-2-4.php" >Задание №1</a></li>
+    <li><a href="Mukhamedyarov/WWW/lab-2-5.php" >Задание №2</a></li>
+    <li><a href="Mukhamedyarov/WWW/lab-2-6.php" >Задание №3</a></li>
+  </ul>
+  
+    <h2>Лабораторная работа №3</h2>
+  <ul>
+    <li><a href="Mukhamedyarov/WWW/s3-1.php" >Задание №1</a></li>
+    <li><a href="Mukhamedyarov/WWW/s3-2.php" >Задание №2</a></li>
+    <li><a href="Mukhamedyarov/WWW/s3-3.php" >Задание №3</a></li>
+    <li><a href="Mukhamedyarov/WWW/s3-4.php" >Задание №4</a></li>
+    <li><a href="Mukhamedyarov/WWW/s3-5.php" >Задание №5</a></li>
+    <li><a href="Mukhamedyarov/WWW/s3-6.php" >Задание №6</a></li>
+  </ul>
+  
+      <h2>Лабораторная работа №4</h2>
+    <ul>
+    <li><a href="Mukhamedyarov/WWW/db/index.php" >Задание c базами данных</a></li>
+  </ul>
+  
+        <h2>Лабораторная работа №5</h2>
+    <ul>
+    <li><a href="Mukhamedyarov/WWW/Lab5-bd/index.php" >Задание c базами данных</a></li>
+  </ul>
+</body>
+</html>
