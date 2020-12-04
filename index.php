0fd09c467f1bcefbf0e6abc62ffaddbd743a63e5
VadimMUKH <nuvare1@gmail.com>
2020-12-04 19:08:27 +0500

    Index
    index-page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
     <h1> Мухамедьяров Вадим А.</h1>
     
-    <h2> Лабораторная работа №1</h2>
+    <h2> Лабораторная работа №1 </h2>
     
   <ul>
     <li><a href="Mukhamedyarov/WWW/lab1-5.php" >Задание №1</a>
@@ -22,7 +22,7 @@
     <li><a href="Mukhamedyarov/WWW/lab-2-6.php" >Задание №3</a></li>
   </ul>
   
-    <h2>Лабораторная работа №3</h2>
+    <h2>Лабораторная работа №3 </h2>
   <ul>
     <li><a href="Mukhamedyarov/WWW/s3-1.php" >Задание №1</a></li>
     <li><a href="Mukhamedyarov/WWW/s3-2.php" >Задание №2</a></li>
@@ -32,12 +32,12 @@
     <li><a href="Mukhamedyarov/WWW/s3-6.php" >Задание №6</a></li>
   </ul>
   
-      <h2>Лабораторная работа №4</h2>
+      <h2>Лабораторная работа №4 </h2>
     <ul>
     <li><a href="Mukhamedyarov/WWW/db/index.php" >Задание c базами данных</a></li>
   </ul>
   
-        <h2>Лабораторная работа №5</h2>
+        <h2>Лабораторная работа №5 </h2>
     <ul>
     <li><a href="Mukhamedyarov/WWW/Lab5-bd/index.php" >Задание c базами данных</a></li>
   </ul>
